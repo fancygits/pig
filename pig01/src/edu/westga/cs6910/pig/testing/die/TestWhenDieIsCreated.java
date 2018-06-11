1c31de88923263645b834fbfcff497b1725c6598
James Luke Johnson <jjohn144@my.westga.edu>
2018-06-11 07:36:25 -0500

    Tested Die constructor and toString. Merged web repository.

---
+++ pig01/src/edu/westga/cs6910/pig/testing/die/TestWhenDieIsCreated.java
@@ -0,0 +1,28 @@
+package edu.westga.cs6910.pig.testing.die;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.Die;
+
+/**
+ * Tests the constructor and toString method of Die
+ * 
+ * @author James Luke Johnson
+ * @version 2018.06.11
+ */
+class TestWhenDieIsCreated {
+
+	/**
+	 * Test to confirm that a new Die object is constructed and returns the correct
+	 * toString statement.
+	 * Test method for {@link edu.westga.cs6910.pig.model.Die#Die(java.util.Random)}.
+	 */
+	@Test
+	void testShouldProduceDieWith1Pip() {
+		Die newDie = new Die(new Random());
+		assertEquals("pips: 1", newDie.toString());
+	}
+}
